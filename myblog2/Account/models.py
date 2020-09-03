8052e7068d4a242dbc5e755991edf46b96d10fc4
Ivan Trofimov <ivantrofimov052@gmail.com>
2020-09-03 13:27:22 +0300

    # commit 3
    in this commit i make get user in the table and checked to there wasnt to be
    simmularity user_names

--- myblog2/Account/models.py
+++ myblog2/Account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 
-class Logins(models.Model):
+class Login (models.Model):
 	user_name = models.CharField(max_length=20)
 	user_password = models.CharField(max_length=65)
--- myblog2/Account/views.py
+++ myblog2/Account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from Account.models import Logins
+from Account.models import Login
 from .hash import Hash
 
 
@@ -14,6 +14,13 @@ def sign_up_handler(request):
 	# get user name and his password
 	user_name = request.GET['user_name']
 	user_password = hash.hash_password(str(request.GET['user_password'][0]))
-	# create this user in table
-	new_user = Logins(user_name=user_name, user_password=user_password)
-	return HttpResponse(user_name + str(user_password))
+	# checked if the user_name is busy 
+	if(Login.objects.filter(user_name = user_name)):
+		# if name is busy
+		return HttpResponse('Name is busy')
+	else:
+		# if name is not busy
+		# create this user in table
+		new_user = Login(user_name=user_name, user_password=user_password)
+		new_user.save()
+		return HttpResponse(user_name + str(user_password))
